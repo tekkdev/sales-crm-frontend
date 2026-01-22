7a24193a75f5ce3a8faba1d07c8b3d6bf455de87
talha-rauf-dev <talharauf.bsee@gmail.com>
2026-01-22 15:12:17 +0500

    fix reducer issue causing build error

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,8 +1,9 @@
+import { combineReducers } from 'redux';
+
 export interface RootState {
-  // todos: RootState.TodoState; // Example of a simple state slice
   router?: any;
 }
 
-export namespace RootState {
-  // export type TodoState = TodoModel[]; // Example of a simple state slice type
-}
+export const rootReducer = combineReducers<RootState>({
+  // Add reducers here
+}) as any;
